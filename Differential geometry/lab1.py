3740b416e1b2146172fd50dfdbfbf374b99dd2a6
roman <roma.vinn@gmail.com>
2020-05-13 17:31:06 +0300

    formated tests v2

--- Differential geometry/lab1.py
+++ Differential geometry/lab1.py
@@ -25,7 +25,7 @@ Pi = np.pi
 
 
 class Curve3D:
-    def __init__(self, x_func: str, y_func: str, z_func: str, t_range: tuple, param=PARAM):
+    def __init__(self, x_func: str, y_func: str, z_func: str, t_range: tuple, param=PARAM) -> None:
         """ Create a 3d curve with r(t) = (x(t), y(t), z(t)) parametrization.
 
         :params x_func, y_func, z_func: must depend on param (default: t)
@@ -52,7 +52,13 @@ class Curve3D:
         fig = plt.figure()
         self._ax = fig.gca(projection='3d')
 
-    def plot(self, neighborhood=None, num=1000):
+    def __repr__(self):
+        return 'r({t}) = ({x}, {y}, {z})'.format(t=self._t, x=self._x_func, y=self._y_func, z=self._z_func)
+
+    def __str__(self):
+        return 'r({t}) = ({x}, {y}, {z})'.format(t=self._t, x=self._x_func, y=self._y_func, z=self._z_func)
+
+    def plot(self, neighborhood=None, num=1000) -> None:
         """ Plot the curve on graphic.
 
             :param neighborhood: if given, plot in the neighborhood of the point
@@ -69,15 +75,12 @@ class Curve3D:
         y = self._y(t)
         z = self._z(t)
 
-        self._ax.plot3D(x, y, z, label='r({t}) = ({x}, {y}, {z})'.format(t=self._t,
-                                                                         x=self._x_func,
-                                                                         y=self._y_func,
-                                                                         z=self._z_func))
+        self._ax.plot3D(x, y, z, label=str(self))
         self._ax.legend()
         set_axes_equal(self._ax)
         plt.show()
 
-    def tangent_vector(self, t: float, plot=False) -> np.array:
+    def tangent_vector(self, t: float, plot=False) -> np.ndarray:
         """ Unit vector tangent to the curve, pointing in the direction of motion at point t.
             τ = r'(t) / |r'(t)|
 
@@ -93,7 +96,7 @@ class Curve3D:
             self._plt_vector(origin, vector, color='r', label='tangent unit vector')
         return vector
 
-    def normal_vector(self, t: float, plot=False) -> np.array:
+    def normal_vector(self, t: float, plot=False) -> np.ndarray:
         """ Normal unit vector at point t.
             ν = [ß, τ]
 
@@ -109,7 +112,7 @@ class Curve3D:
             self._plt_vector(origin, nu, color='g', label='normal unit vector')
         return nu
 
-    def binormal_vector(self, t: float, plot=False) -> np.array:
+    def binormal_vector(self, t: float, plot=False) -> np.ndarray:
         """ Binormal unit vector at point t, cross product of T and N.
             ß = [r'(t), r''(t)] / |[r'(t), r''(t)]|
 
@@ -223,7 +226,7 @@ class Curve3D:
             module += f**2
         return np.sqrt(module)
 
-    def _plt_vector(self, origin, vector, color='b', label=None):
+    def _plt_vector(self, origin, vector, color='b', label=None) -> None:
         """ Helper method to add vector from origin point to self._ax figure """
         assert len(origin) == 3, 'origin must contain 3 coordinates'
         assert len(vector) == 3, 'vector must contain 3 coordinates'
@@ -239,7 +242,7 @@ class Curve3D:
                 float(self._y_func.subs(self._t, t)),
                 float(self._z_func.subs(self._t, t)))
 
-    def _validate_t(self, t):
+    def _validate_t(self, t) -> None:
         """ Helper method to check weather """
         assert self._t_lower <= t <= self._t_upper, 't not in the t_range'
 
@@ -254,11 +257,11 @@ class Curve3D:
             raise AssertionError('vector1 and vector2 must be non-collinear')
         return plane
 
-    def _get_derivative(self, t, order=1) -> tuple:
+    def _get_derivative(self, t, order=1) -> np.ndarray:
         """ Helper method to find order'th derivative of r at given point t"""
-        return (float(diff(self._x_func, self._t, order).subs(self._t, t)),
-                float(diff(self._y_func, self._t, order).subs(self._t, t)),
-                float(diff(self._z_func, self._t, order).subs(self._t, t)))
+        return np.array([float(diff(self._x_func, self._t, order).subs(self._t, t)),
+                         float(diff(self._y_func, self._t, order).subs(self._t, t)),
+                         float(diff(self._z_func, self._t, order).subs(self._t, t))])
 
 
 def set_axes_equal(ax):
@@ -292,29 +295,34 @@ def set_axes_equal(ax):
 
 # testing
 if __name__ == '__main__':
-    # # test 1
+    # test 1
     curve = Curve3D('sin(t)', 'cos(t)', 'tan(t)', (0, 5))
     point = Pi / 4
-    tangent_v = curve.tangent_vector(point, plot=True)
     normal_v = curve.normal_vector(point, plot=True)
     binormal_v = curve.binormal_vector(point, plot=True)
-    print(tangent_v, normal_v, binormal_v)
-    curve.plot(neighborhood=(Pi / 12, Pi / 3))
+    print('For curve {} at point {}:'.format(curve, point))
+    print('\ttangent unit vector: {}'.format(curve.tangent_vector(point, plot=True)))
+    print('\tnormal unit vector: {}'.format(curve.normal_vector(point, plot=True)))
+    print('\tbinormal unit vector: {}'.format(curve.binormal_vector(point, plot=True)))
 
-    # # test 2
+    # test 2
     curve2 = Curve3D('2*(t-sin(t))', '2*(t-cos(t))', '8*cos(t/2)', (-Pi, Pi))
     point2 = 0
-    print(curve2.curvature(point2))
-    print(curve2.torsion(point2))
+    print('\nFor curve {} at point {}:'.format(curve2, point2))
+    print('\tcurvature: {}'.format(curve2.curvature(point2)))
+    print('\ttorsion: {}'.format(curve2.torsion(point2)))
+    osc_sphere, osc_plane = curve2.osculating_circle(point2)
+    print('\t# osculating circle <=> intersection of osculating sphere and osculating plane')
+    print('\tosculating sphere: {} = 0'.format(osc_sphere))
+    print('\tosculating plane: {} = 0'.format(osc_plane))
 
-    # # test 3
+    # test 3
     curve3 = Curve3D('t', 't**3', 't**2+1', (0, 2))
     point3 = 1
-    print('Osculating plane at P({point}): {equation} = 0'.format(point=point3,
-                                                                  equation=curve3.osculating_plane(point3)))
-    print('Normal plane at P({point}): {equation} = 0'.format(point=point3,
-                                                              equation=curve3.normal_plane(point3)))
-    print('Reference plane at P({point}): {equation} = 0'.format(point=point3,
-                                                                 equation=curve3.reference_plane(point3)))
-    # test 4
-    print(curve2.osculating_circle(point2))
+    print('\nFor curve {} at point {}:'.format(curve3, point3))
+    print('\tosculating plane: {} = 0'.format(curve3.osculating_plane(point3)))
+    print('\tnormal plane: {} = 0'.format(curve3.normal_plane(point3)))
+    print('\treference plane: {} = 0'.format(curve3.reference_plane(point3)))
+
+    # test 1 - plotting
+    curve.plot(neighborhood=(Pi / 12, Pi / 3))
